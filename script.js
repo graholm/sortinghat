707a45108c988ca233f4ebfea697097caa10170b
graholm <graholm@nuevaschool.org>
2016-12-14 09:27:54 -0800

    Classadoo autocommit

---
+++ script.js
@@ -0,0 +1,34 @@
+var nameInput = $("#name-input")
+var sortButton = $("#sort-button")
+var enrolledStudentList = $(".enrolled-students")
+
+var houses = [
+  "Ravenclaw",
+  "Hufflepuff",
+  "Gryfindor",
+  "Slytherin"
+]
+
+function chooseRandomHouse() {
+  var index = Math.floor(Math.random() * houses.length)
+
+  return houses[index]
+}
+
+sortButton.click(function() {
+  var name = nameInput.val()
+  console.log (name)
+
+  addListItem("enrolledStudents", {
+    name: name,
+    house: chooseRandomHouse()
+  })
+})
+
+onNewListItem("enrolledStudents", function(student) {
+  console.log("student", student)
+
+  var newStudentElement = $("<div>" + Name: +student.name+ House: +student.house +"</div>")
+  
+  enrolledStudentList.append(newStudentElement)
+})
